ddd130df66cae313db5312e3b1cb292e727e201a
lyy97 <867892924@qq.com>
2019-06-19 16:56:36 +0800

    开始界面与音乐设置界面UI

---
+++ Assets/Scripts/UI/mycanvas.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mycanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CanvasGroup cg;
+    private float alpha = 1.0f;
+    private float alphaSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cg = this.transform.GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alpha != cg.alpha)
+        {
+            cg.alpha = Mathf.Lerp(cg.alpha, alpha, alphaSpeed * Time.deltaTime);
+            if (Mathf.Abs(alpha - cg.alpha) <= 0.01)
+            {
+                cg.alpha = alpha;
+            }
+        }
+    }
+
+    public void Show()
+    {
+        alpha = 1;
+
+        cg.blocksRaycasts = true;//可以和该UI对象交互
+
+        cg.interactable = true;
+    }
+    public void Show1()
+    {
+
+        cg.blocksRaycasts = true;//可以和该UI对象交互
+
+        cg.interactable = true;
+    }
+
+    public void Hide()
+    {
+        alpha = 0;
+
+        cg.blocksRaycasts = false;//不可以和该UI对象交互
+
+        cg.interactable = false;
+    }
+
+    public void Hide1()
+    {
+        cg.blocksRaycasts = false;//不可以和该UI对象交互
+
+        cg.interactable = false;
+    }
+}
---
+++ Assets/Scripts/UI/myscroll.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class myscroll : MonoBehaviour,IBeginDragHandler,IEndDragHandler
+{
+    private ScrollRect mscroll;
+    private float[] page = new float[] { 0, 0.5f, 1 };
+    private float slow = 0;
+    public bool flag;
+    public Toggle[] mtoggle;
+    myImage mimage;
+    myimage2 mimage1;
+    myimage2 mimage2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mscroll = GetComponent<ScrollRect>();
+        mimage = GameObject.Find("level_1").GetComponent<myImage>();
+        mimage1 = GameObject.Find("level_2").GetComponent<myimage2>();
+        mimage2 = GameObject.Find("level_3").GetComponent<myimage2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flag==false)
+        {
+            mscroll.horizontalNormalizedPosition = Mathf.Lerp(mscroll.horizontalNormalizedPosition, slow, Time.deltaTime * 4);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        flag = true;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        flag = false;
+        float pos = mscroll.horizontalNormalizedPosition;
+        int index = 0;
+        float offset = Mathf.Abs(page[index] - pos);
+        for (int i = 1; i < page.Length; i++)
+        {
+            float offsetpos = Mathf.Abs(page[i] - pos);
+            if (offsetpos < offset)
+            {
+                index = i;
+                offset = offsetpos;
+            }
+        }
+        slow = page[index];
+        mtoggle[index].isOn = true;
+    }
+
+  
+    public void OnClick2(bool isOn)
+    {
+        if (isOn)
+        {
+            slow = page[1];
+            mimage.Hide();
+            mimage2.Hide();
+            mimage1.Show();
+
+        }
+    }
+    public void OnClick3(bool isOn)
+    {
+        if (isOn)
+        {
+            slow = page[2];
+            mimage.Hide();
+            mimage1.Hide();
+            mimage2.Show();
+        }
+    }
+    public void OnClick1(bool isOn)
+    {
+        if (isOn)
+        {
+            slow = page[0];
+            mimage.Show();
+            mimage1.Hide();
+            mimage2.Hide();
+
+        }
+    }
+}
