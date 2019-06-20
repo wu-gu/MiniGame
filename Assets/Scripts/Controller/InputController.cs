306ca736c804388910b9480a710ce0a5c716d465
wu-gu <841904352@qq.com>
2019-06-20 14:46:36 +0800

    输入控制分离为机关控制与人物控制

--- Assets/Scripts/Controller/InputController.cs
+++ Assets/Scripts/Controller/InputController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 namespace MiniGame
 {
-
     public class InputController
     {
-        /// <summary>
-        /// 手动更新
-        /// </summary>
+        public LayerMask questLayerMask;
+
+        public float questRaycastDistance = 0.1f;
+
+
         public void onUpdate()
         {
+            if (Input.touchCount>0)
+            {
+                //先判断是否触摸某个机关
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+                RaycastHit2D hit = Physics2D.Raycast(touchPos, Vector2.zero, questRaycastDistance, questLayerMask.value);
+                if (hit.collider != null)
+                {
+                    QuestController.Instance.FireQuestBehavior(hit.collider.gameObject.ToString());
+                }
+                else
+                {
+                    //没有点击机关，则进入人物行走判断
 
+                }
+            }
         }
     }
 }
+
---
+++ Assets/Scripts/Interactables/QuestBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 机关行为接口
+/// </summary>
+public interface QuestBehavior
+{
+    //public enum BehaviorType
+    //{
+    //    Traslate,
+    //    Rotate,
+    //    Scale
+    //}
+
+    //public BehaviorType behaviorType;
+
+
+    void OnUpdate();
+
+}
