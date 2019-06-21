e7a12ebd2c0f583d5bd1d25651f8fa5e841b9816
lyy97 <867892924@qq.com>
2019-06-21 09:54:57 +0800

    UI合并进入框架

--- Assets/Scripts/UI/buttonscript.cs
+++ Assets/Scripts/UI/buttonscript.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class buttonscript : MonoBehaviour
 {
@@ -13,41 +14,43 @@ public class buttonscript : MonoBehaviour
     mycanvas2 com3;
     public ParticleSystem par;
 
+
     void Start()
     {
         com = GameObject.Find("init").GetComponent<mycanvas>();
         com1 = GameObject.Find("start").GetComponent<mycanvas1>();
         com2 = GameObject.Find("set").GetComponent<mycanvas2>();
         com3 = GameObject.Find("endshow").GetComponent<mycanvas2>();
-
+       
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClickStar()
     {
+        
         com.Hide();
         com1.Show();
-        com2.Hide();
         par.gameObject.SetActive(false);
     }
 
     public void OnClickReturn()
     {
+
         com1.Hide();
         com.Show();
-        com2.Hide();
         par.gameObject.SetActive(true);
     }
 
     public void OnClickSet()
     {
-        com1.Hide();
+  
         com2.Show();
         com.Hide();
         par.gameObject.SetActive(false);
@@ -55,6 +58,7 @@ public class buttonscript : MonoBehaviour
 
     public void OnClickReturn1()
     {
+
         com1.Hide();
         com.Show();
         com2.Hide();
@@ -63,34 +67,40 @@ public class buttonscript : MonoBehaviour
 
     public void OnClickEND()
     {
+
         com3.Show();
-        com1.Hide();
-        com2.Hide();
     }
 
     public void OnClickENDSure()
     {
+ 
         com3.Hide();
     }
 
     public void OnClickENDCancel()
     {
+
         com3.Hide();
     }
 
     public void OnClickLevel_1()
     {
+ 
         print("wdnmd!");
 
         
     }
     public void OnClickLevel_2()
     {
+
         print("2222222222!");
     }
     public void OnClickLevel_3()
     {
+
         print("333333333!");
     }
+
+
 
 }
