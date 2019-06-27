679393ed3686e1044f53d94bca0c9f72a0048f8f
XStrachey <1094397651@qq.com>
2019-06-27 10:01:06 +0800

    场景1-3（渡河情景）开发，添加倒影相机脚本

---
+++ Assets/Scripts/Scene1/Scene1-3/MagpieQuestFiredAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagpieQuestFiredAnimations : MonoBehaviour
+{
+    GameObject m_willow;
+    GameObject m_flyingMagpie;
+    public IEnumerator MagpieQuestFired()
+    {
+        WillowShake();
+        yield return new WaitForSeconds(2f);
+        FlyingMagpieAppear();
+    }
+
+    void WillowShake()
+    {
+        m_willow = GameObject.Find("Willow");
+        m_willow.GetComponent<Animator>().SetBool("MagpieQuestFired", true);
+    }
+
+    void FlyingMagpieAppear()
+    {
+        m_flyingMagpie = GameObject.Find("FlyingMagpie");
+        m_flyingMagpie.GetComponent<Animator>().SetBool("MagpieQuestFired", true);
+    }
+}
