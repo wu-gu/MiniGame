b3967f0768966046056c009ccde15fc8cc761818
Ecineco <simon323@126.com>
2019-06-28 16:48:21 +0800

    合并水车场景

--- Assets/Scripts/Scene1/TestWaterWheelQuest.cs
+++ Assets/Scripts/Scene1/TestWaterWheelQuest.cs
@@ -12,6 +12,8 @@ public class TestWaterWheelQuest : MonoBehaviour, QuestBehavior
     private int currState;
     private ChangeShadows changeShadows;
     private ParticleSystem m_clearSign;
+    private CircleCollider2D m_clearSignCollider;
+    private SelfRotate m_selfRotate;
 
     void Awake()
     {
@@ -20,11 +22,14 @@ public class TestWaterWheelQuest : MonoBehaviour, QuestBehavior
         currState = 0;
         changeShadows = GameObject.Find("Shadows").GetComponent<ChangeShadows>();
         m_clearSign = GameObject.Find("Clear Sign").GetComponent<ParticleSystem>();
+        m_clearSignCollider = GameObject.Find("Clear Sign").GetComponent<CircleCollider2D>();
+        m_selfRotate = GetComponent<SelfRotate>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // PC --stable
         if (Input.GetMouseButton(0))
         {
             Vector2 touchPos = (Vector2)(transform.position);
@@ -36,6 +41,7 @@ public class TestWaterWheelQuest : MonoBehaviour, QuestBehavior
 
             float angle = Vector3.Angle(preDirectionVec3, currDirectionVec3);
             Vector3 normal = Vector3.Cross(preDirectionVec3, currDirectionVec3);
+
             //计算顺时针还是逆时针
             angle *= Mathf.Sign(Vector3.Dot(normal, transform.forward));
             transform.Rotate(new Vector3(0, 0, angle));
@@ -48,8 +54,45 @@ public class TestWaterWheelQuest : MonoBehaviour, QuestBehavior
                 currState = state;
                 changeShadows.SetUpNewShadowState(state);
             }
-            
+
         }
+
+        // Android --stable
+        //if (Input.touchCount > 0)
+        //{
+        //    Touch currTouch = Input.GetTouch(0);
+        //    Vector2 touchPos = (Vector2)(transform.position);
+        //    touchPos = Camera.main.ScreenToWorldPoint(currTouch.position);
+        //    Vector2 currDirection = touchPos - (Vector2)(transform.position);
+
+        //    if(currTouch.phase == TouchPhase.Moved)
+        //    {
+        //        Debug.Log("Moved");
+        //        Vector3 preDirectionVec3 = new Vector3(m_preDirection.x, m_preDirection.y, transform.position.z).normalized;
+        //        Vector3 currDirectionVec3 = new Vector3(currDirection.x, currDirection.y, transform.position.z).normalized;
+
+        //        Debug.Log("PreDirection" + preDirectionVec3);
+        //        Debug.Log("CurrDirection" + currDirectionVec3);
+
+        //        float angle = Vector3.Angle(preDirectionVec3, currDirectionVec3);
+        //        Vector3 normal = Vector3.Cross(preDirectionVec3, currDirectionVec3);
+        //
+        //        //计算顺时针还是逆时针
+        //        angle *= Mathf.Sign(Vector3.Dot(normal, transform.forward));
+        //        Debug.Log(angle);
+        //        transform.Rotate(new Vector3(0, 0, angle));
+        //        m_preDirection = currDirection;
+
+        //        angleCounter += angle;
+        //        int state = (int)(Mathf.Abs(angleCounter) / 720);
+        //        if (state != currState)
+        //        {
+        //            currState = state;
+        //            changeShadows.SetUpNewShadowState(state);
+        //        }
+        //    }
+        //} 
+
         else
         {
             if (currState >= 4)
@@ -57,14 +100,31 @@ public class TestWaterWheelQuest : MonoBehaviour, QuestBehavior
                 GetComponent<CircleCollider2D>().enabled = false;
                 QuestController.Instance.UnRegisterQuest(gameObject.ToString());
                 m_clearSign.Play();
+                m_clearSignCollider.enabled = true;
             }
+            m_selfRotate.enabled = true;
             this.enabled = false;
         }
     }
 
     public void OnUpdate()
     {
+        m_selfRotate.enabled = false;
         this.enabled = true;
+
+        Vector2 touchPos = (Vector2)(transform.position);
+
+        // Android --stable
+        //Touch currTouch = Input.GetTouch(0);
+        //touchPos = Camera.main.ScreenToWorldPoint(currTouch.position);
+
+        // PC --stable
+        Vector2 currTouch = (Vector2)Input.mousePosition;
+        touchPos = Camera.main.ScreenToWorldPoint(currTouch);
+
+        Vector2 currDirection = touchPos - (Vector2)(transform.position);
+        m_preDirection = currDirection;
+        Debug.Log("Began");
     }
 
 }
---
+++ Assets/Test/TestWaterWheel/TestClearSign.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClearSign : MonoBehaviour
+{
+    private ParticleSystem m_particleSystem;
+    private CircleCollider2D m_circleCollider2D;
+
+    void Awake()
+    {
+        m_particleSystem = GetComponent<ParticleSystem>();
+        m_circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    void OnMouseDown()
+    {
+        m_particleSystem.Stop();
+        m_particleSystem.Clear();
+        m_circleCollider2D.enabled = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        m_particleSystem.Stop();
+        m_particleSystem.Clear();
+        m_circleCollider2D.enabled = false;
+    }
+}
