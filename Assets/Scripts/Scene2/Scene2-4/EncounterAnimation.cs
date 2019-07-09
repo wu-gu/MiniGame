c445be306b93de36aed6aeab9be06e565b83f440
XStrachey <1094397651@qq.com>
2019-07-09 20:33:23 +0800

    增加Level2-4结束男女相遇动画

---
+++ Assets/Scripts/Scene2/Scene2-4/EncounterAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterAnimation : MonoBehaviour
+{
+    private GameObject m_shield;
+    private GameObject m_girl;
+    private GameObject m_boy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_girl = GameObject.FindGameObjectWithTag("Girl");
+        m_boy = GameObject.Find("Couple");
+        m_shield = GameObject.Find("Shield");
+    }
+
+    void DestoryShield()
+    {
+        GameObject.Destroy(m_shield);
+    }
+
+    void MeetingTriggerFired()
+    {
+        m_boy.GetComponent<Animator>().SetBool("MeetingTriggerFired", true);
+        m_girl.GetComponent<Animator>().SetBool("MeetingTriggerFired", true);
+    }
+}
