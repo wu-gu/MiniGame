8689a3e2c56ea90472d104ff07af3bd58df6c156
wu-gu <841904352@qq.com>
2019-07-11 21:53:43 +0800

    画中缘最终版(Android)

--- Assets/Scripts/Scene0/ScaleMask.cs
+++ Assets/Scripts/Scene0/ScaleMask.cs
@@ -50,78 +50,78 @@ namespace MiniGame
         {
             //Test scaling in pc platform using mouse scroll wheel
 
-//#if UNITY_STANDALONE_WIN
-            float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
-            Vector3 scaleOffset = new Vector3(mouseScrollWheel, mouseScrollWheel, mouseScrollWheel);
-            Vector3 currentScale = this.transform.localScale;
+            //#if UNITY_STANDALONE_WIN
+            //float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+            //Vector3 scaleOffset = new Vector3(mouseScrollWheel, mouseScrollWheel, mouseScrollWheel);
+            //Vector3 currentScale = this.transform.localScale;
 
-            if (scaleOffset.magnitude > 0)
-                circleCollider2D.enabled = false;
+            //if (scaleOffset.magnitude > 0)
+            //    circleCollider2D.enabled = false;
 
-            if (scaleOffset.x + currentScale.x <= originalScale.x || scaleOffset.y + currentScale.y <= originalScale.y || scaleOffset.z + currentScale.z <= originalScale.z)
-            {
-                this.transform.localScale = originalScale;
-                return;
-            }
-            else
-            {
-                this.transform.localScale += scaleOffset;
-            }
-//#endif
-
-
-            // Real usage in Android platform
-#if UNITY_ANDROID
-            // Nothing to process when one or less finger is detected
-            //if (Input.touchCount <= 1)
+            //if (scaleOffset.x + currentScale.x <= originalScale.x || scaleOffset.y + currentScale.y <= originalScale.y || scaleOffset.z + currentScale.z <= originalScale.z)
             //{
-            //    // Trick is scale back, once the player loses control of the trick and has not reached end point
             //    this.transform.localScale = originalScale;
-            //    circleCollider2D.enabled = true;
-            //    if (Input.touchCount < 1)
-            //        this.enabled = false; // No touchCount(--At least not consistent on mask)
             //    return;
             //}
             //else
             //{
-            //    // Process mask scaling when two fingers are detected, more than two: process as two
-
-            //    Touch curr1 = Input.GetTouch(0);
-            //    Touch curr2 = Input.GetTouch(1);
-
-            //    // Ignore the process when the second finger is just right on touch, in case of mis-process
-            //    if (curr2.phase == TouchPhase.Began)
-            //    {
-            //        pre2 = curr2;
-            //        pre1 = curr1;
-            //        return;
-            //    }
-
-            //    // Process when the user input is clearly the act of scaling
-            //    else
-            //    {
-            //        // Disable collider component when scale starts, or player will be pushed out 
-            //        circleCollider2D.enabled = false;
-
-            //        float preDist = Vector2.Distance(pre1.position, pre2.position);
-            //        float currDist = Vector2.Distance(curr1.position, curr2.position);
-
-            //        // Scale distance
-            //        float offset = currDist - preDist;
-
-            //        // Calculate scale offset and new scale vector per frame
-            //        float scaleFactor = offset / 300f;
-            //        Vector3 localScale = this.transform.localScale;
-            //        Vector3 scale = new Vector3(localScale.x + scaleFactor, localScale.y + scaleFactor, localScale.z + scaleFactor);
-
-            //        // Certain limitation of smallest scale size
-            //        if (scale.x >= originalScale.x && scale.y >= originalScale.y && scale.z >= originalScale.z)
-            //            transform.localScale = scale;
-            //        pre1 = curr1;
-            //        pre2 = curr2;
-
-            //    }
+            //    this.transform.localScale += scaleOffset;
             //}
+            //#endif
+
+
+            // Real usage in Android platform
+#if UNITY_ANDROID
+            //Nothing to process when one or less finger is detected
+            if (Input.touchCount <= 1)
+            {
+                // Trick is scale back, once the player loses control of the trick and has not reached end point
+                this.transform.localScale = originalScale;
+                circleCollider2D.enabled = true;
+                if (Input.touchCount < 1)
+                    this.enabled = false; // No touchCount(--At least not consistent on mask)
+                return;
+            }
+            else
+            {
+                // Process mask scaling when two fingers are detected, more than two: process as two
+
+                Touch curr1 = Input.GetTouch(0);
+                Touch curr2 = Input.GetTouch(1);
+
+                // Ignore the process when the second finger is just right on touch, in case of mis-process
+                if (curr2.phase == TouchPhase.Began)
+                {
+                    pre2 = curr2;
+                    pre1 = curr1;
+                    return;
+                }
+
+                // Process when the user input is clearly the act of scaling
+                else
+                {
+                    // Disable collider component when scale starts, or player will be pushed out 
+                    circleCollider2D.enabled = false;
+
+                    float preDist = Vector2.Distance(pre1.position, pre2.position);
+                    float currDist = Vector2.Distance(curr1.position, curr2.position);
+
+                    // Scale distance
+                    float offset = currDist - preDist;
+
+                    // Calculate scale offset and new scale vector per frame
+                    float scaleFactor = offset / 300f;
+                    Vector3 localScale = this.transform.localScale;
+                    Vector3 scale = new Vector3(localScale.x + scaleFactor, localScale.y + scaleFactor, localScale.z + scaleFactor);
+
+                    // Certain limitation of smallest scale size
+                    if (scale.x >= originalScale.x && scale.y >= originalScale.y && scale.z >= originalScale.z)
+                        transform.localScale = scale;
+                    pre1 = curr1;
+                    pre2 = curr2;
+
+                }
+            }
 #endif
 
             //common
--- Assets/Scripts/Scene2/Scene2-1/CameraScale.cs
+++ Assets/Scripts/Scene2/Scene2-1/CameraScale.cs
@@ -44,76 +44,76 @@ namespace MiniGame
             float currentSize = m_camera.orthographicSize;
 
             // PC -- stable
-            float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+            //float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
 
-            if (mouseScrollWheel + currentSize <= m_orthograp)
-            {
-                m_camera.orthographicSize = m_orthograp;
-                return;
-            }
-            else
-            {
-                if (mouseScrollWheel + currentSize >= m_dest && m_firstTime)
-                {
-                    m_camera.orthographicSize = m_dest;
-                    m_animator.enabled = true;
-                    m_animator.SetTrigger(IsScaleTime);
-                    m_firstTime = false;
-                }
-                else
-                    m_camera.orthographicSize = currentSize + mouseScrollWheel;
-            }
+            //if (mouseScrollWheel + currentSize <= m_orthograp)
+            //{
+            //    m_camera.orthographicSize = m_orthograp;
+            //    return;
+            //}
+            //else
+            //{
+            //    if (mouseScrollWheel + currentSize >= m_dest && m_firstTime)
+            //    {
+            //        m_camera.orthographicSize = m_dest;
+            //        m_animator.enabled = true;
+            //        m_animator.SetTrigger(IsScaleTime);
+            //        m_firstTime = false;
+            //    }
+            //    else
+            //        m_camera.orthographicSize = currentSize + mouseScrollWheel;
+            //}
 
             // Android -- stable
-            //if (Input.touchCount == 2)
-            //{
-            //    Touch curr1 = Input.GetTouch(0);
-            //    Touch curr2 = Input.GetTouch(1);
-            //    if (curr2.phase == TouchPhase.Began)
-            //    {
-            //        pre2 = curr2;
-            //        pre1 = curr1;
-            //        return;
-            //    }
+            if (Input.touchCount == 2)
+            {
+                Touch curr1 = Input.GetTouch(0);
+                Touch curr2 = Input.GetTouch(1);
+                if (curr2.phase == TouchPhase.Began)
+                {
+                    pre2 = curr2;
+                    pre1 = curr1;
+                    return;
+                }
 
-            //    // Process when the user input is clearly the act of scaling
-            //    else
-            //    {
-            //        // Disable collider component when scale starts, or player will be pushed out 
+                // Process when the user input is clearly the act of scaling
+                else
+                {
+                    // Disable collider component when scale starts, or player will be pushed out 
 
-            //        float preDist = Vector2.Distance(pre1.position, pre2.position);
-            //        float currDist = Vector2.Distance(curr1.position, curr2.position);
+                    float preDist = Vector2.Distance(pre1.position, pre2.position);
+                    float currDist = Vector2.Distance(curr1.position, curr2.position);
 
-            //        // Scale distance
-            //        float offset = preDist - currDist;
+                    // Scale distance
+                    float offset = preDist - currDist;
 
-            //        // Calculate scale offset and new scale vector per frame
-            //        float scaleFactor = offset / 2000f;
+                    // Calculate scale offset and new scale vector per frame
+                    float scaleFactor = offset / 2000f;
 
-            //        if (scaleFactor + currentSize <= m_orthograp)
-            //        {
-            //            m_camera.orthographicSize = m_orthograp;
-            //            return;
-            //        }
-            //        else
-            //        {
-            //            if (scaleFactor + currentSize >= m_dest)
-            //            {
-            //                m_camera.orthographicSize = m_dest;
-            //                m_animator.enabled = true;
-            //                m_animator.SetTrigger(IsScaleTime);
-            //                m_animator.enabled = true;
+                    if (scaleFactor + currentSize <= m_orthograp)
+                    {
+                        m_camera.orthographicSize = m_orthograp;
+                        return;
+                    }
+                    else
+                    {
+                        if (scaleFactor + currentSize >= m_dest)
+                        {
+                            m_camera.orthographicSize = m_dest;
+                            m_animator.enabled = true;
+                            m_animator.SetTrigger(IsScaleTime);
+                            m_animator.enabled = true;
 
-            //            }
-            //            else
-            //                m_camera.orthographicSize = currentSize + scaleFactor;
-            //        }
+                        }
+                        else
+                            m_camera.orthographicSize = currentSize + scaleFactor;
+                    }
 
-            //        pre1 = curr1;
-            //        pre2 = curr2;
+                    pre1 = curr1;
+                    pre2 = curr2;
 
-            //    }
-            //}
+                }
+            }
 
             //common
             AnimatorStateInfo animatorInfo;
