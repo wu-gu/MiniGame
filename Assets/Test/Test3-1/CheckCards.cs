e1951c64cc11712ebb6e606276df91b2972d96f0
Ecineco <simon323@126.com>
2019-08-02 16:23:47 +0800

    添加3-1逻辑

---
+++ Assets/Test/Test3-1/CheckCards.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCards : MonoBehaviour
+{
+    private MoveCard[] cardScripts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardScripts = new MoveCard[4];
+        cardScripts[0] = GameObject.Find("Card_1").GetComponent<MoveCard>();
+        cardScripts[1] = GameObject.Find("Card_2").GetComponent<MoveCard>();
+        cardScripts[2] = GameObject.Find("Card_3").GetComponent<MoveCard>();
+        cardScripts[3] = GameObject.Find("Card_4").GetComponent<MoveCard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int correctCounter = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (cardScripts[i].GetCurrentZone() == MoveCard.AtZone.NONE + i + 1)
+                correctCounter++;
+        }
+        if (correctCounter == 4)
+        {
+            Debug.Log("3-1 Success");
+            for (int i = 0; i < 4; i++)
+                QuestController.Instance.UnRegisterQuest(cardScripts[i].gameObject.ToString());
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Test/Test3-1/MoveCard.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCard : MonoBehaviour, QuestBehavior
+{
+    public enum AtZone
+    {
+        NONE, ZONE1, ZONE2, ZONE3, ZONE4,
+    }
+
+    private AtZone currentZone;
+    private Collider2D[] zones;
+    private Vector3 offSet;
+    private bool moveToCenter;
+    private Vector3 boxCenter;
+    private Vector3 originalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuestController.Instance.RegisterQuest(gameObject.ToString(), this);
+        currentZone = AtZone.NONE;
+        zones = new Collider2D[4];
+        zones[0] = GameObject.Find("CardZone_1").GetComponent<Collider2D>();
+        zones[1] = GameObject.Find("CardZone_2").GetComponent<Collider2D>();
+        zones[2] = GameObject.Find("CardZone_3").GetComponent<Collider2D>();
+        zones[3] = GameObject.Find("CardZone_4").GetComponent<Collider2D>();
+        offSet = Vector3.zero;
+        moveToCenter = true;
+        originalPos = this.transform.position;
+        this.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        if(Input.GetMouseButton(0))
+            this.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - offSet;
+        else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if(zones[i].bounds.Contains(this.transform.position))
+                {
+                    this.transform.position = zones[i].bounds.center;
+                    currentZone = AtZone.NONE + i + 1;
+                    break;
+                }
+                if (i == 3)
+                    currentZone = AtZone.NONE;
+            }
+            if (currentZone == AtZone.NONE)
+                this.transform.position = originalPos;
+            this.enabled = false;
+        }
+    }
+
+    public AtZone GetCurrentZone()
+    {
+        return currentZone;
+    }
+
+    public void OnUpdate()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offSet = mousePos - this.transform.position;
+        this.enabled = true;
+    }
+}
